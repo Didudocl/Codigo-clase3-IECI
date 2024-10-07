03cfde47f5f078f84fdc217c10f5dd04fb1c7987
LuisFernandez <luis.fernandez2101@alumnos.ubiobio.cl>
2024-10-07 19:48:39 -0300

    Tarea3_IECI

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -37,3 +37,63 @@ export async function getUserService(id) {
         console.error("Error al obtener el usuario:", error);
     }
 }
+//userssss
+
+export async function getUsersService() {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const users = await userRepository.find();
+
+        if (!users || users.length === 0) {
+            return null;
+        }
+
+        return users;
+    } catch (error) {
+        console.error("Error al obtener los usuarios:", error);
+    }
+}
+
+//update
+export async function updateUserService(id, dataUser) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const userFound = await userRepository.findOne({
+            where: { id }
+        });
+
+        if (!userFound) {
+            return null;
+        }
+
+        await userRepository.update(id, dataUser);
+
+        const userUpdated = await userRepository.findOne({
+            where: { id }
+        });
+        return userUpdated;
+    } catch (error) {
+        console.error("Error al actualizar el usuario:", error);
+    }
+}
+//delete
+export async function deleteUserService(id) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const userFound = await userRepository.findOne({
+            where: { id }
+        });
+
+        if (!userFound) {
+            return null;
+        }
+        await userRepository.remove(userFound);
+
+        return userFound;
+    } catch (error) {
+        console.error("Error al eliminar el usuario:", error);
+    }
+}
