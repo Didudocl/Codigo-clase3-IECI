b94d24fa31777adcf0aa089f0600250a22657137
benjivoll <benjivoll@gmail.com>
2024-10-07 11:44:23 -0300

    ADD user.services

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -20,6 +20,20 @@ export async function createUserService(dataUser) {
     }
 }
 
+/**
+ * Obtiene todos los usuarios de la base de datos
+ */
+export async function getUsersService() {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const users = await userRepository.find();
+        return users;
+    } catch (error) {
+        console.error("Error al obtener los usuarios:", error);
+    }
+}
+
 export async function getUserService(id) {
     try {
         const userRepository = AppDataSource.getRepository(User);
@@ -35,5 +49,43 @@ export async function getUserService(id) {
         return userFound;
     } catch (error) {
         console.error("Error al obtener el usuario:", error);
+    }
+}
+
+export async function updateUserService() {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const userToUpdate = await userRepository.findOne({ where: { id } });
+
+        if (!userToUpdate) {
+            return null;
+        }
+
+        userRepository.merge(userToUpdate, dataUser);
+
+        const updateUser = await userRepository.save(userToUpdate);
+
+        return updateUser;
+    } catch (error) {
+        console.error("Error al obtener el usuario:", error);
+    }
+}
+
+export async function deleteUserService(id) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const userToDelete = await userRepository.findOne({ where: { id } });
+
+        if (!userToDelete) {
+            return null;
+        }
+
+        await userRepository.remove(userToDelete);
+        
+        return true;
+    } catch (error) {
+        console.error("Error al eliminar el usuario:", error);
     }
 }
