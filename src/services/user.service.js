aed76352501d07a54419dca99cbb9018d476248e
Matias <138421461+MatiLakes@users.noreply.github.com>
2024-10-06 21:23:28 -0300

    Update user.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -37,3 +37,63 @@ export async function getUserService(id) {
         console.error("Error al obtener el usuario:", error);
     }
 }
+
+export async function getUsersService() {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const users = await userRepository.find();
+
+        return users;
+    } catch (error) {
+        console.error('Error al obtener usuarios: ', error);
+        throw error; 
+    }
+}
+
+export async function updateUserService(id, userData) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const userFound = await userRepository.findOne({
+            where: { id }
+        });
+
+        if (!userFound) {
+            return null;
+        }
+
+        await userRepository.update(id, userData);
+
+        const updatedUser = await userRepository.findOne({
+            where: { id }
+        });
+
+        return updatedUser;
+    } catch (error) {
+        console.error('Error al actualizar un usuario: ', error);
+        throw error; 
+    }
+}
+
+
+export async function deleteUserService(id) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const userFound = await userRepository.findOne({
+            where: { id }
+        });
+
+        if (!userFound) {
+            return null;
+        }
+
+        await userRepository.remove(userFound);
+
+        return userFound; 
+    } catch (error) {
+        console.error('Error al eliminar un usuario: ', error);
+        throw error; 
+    }
+}
