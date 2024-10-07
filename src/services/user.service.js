0b6ed1265a9d52e09f7753361989204117c58c8b
AnthonyMartinHeart <138081463+AnthonyMartinHeart@users.noreply.github.com>
2024-10-06 21:22:15 -0300

    Update user.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -37,3 +37,41 @@ export async function getUserService(id) {
         console.error("Error al obtener el usuario:", error);
     }
 }
+
+export async function getUsersService() {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+        const users = await userRepository.find();
+        return users;
+    } catch (error) {
+        console.error("Error al obtener los usuarios:", error);
+    }
+}
+
+export async function updateUserService(id, dataUser) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+        const userFound = await userRepository.findOne({ where: { id } });
+
+        if (!userFound) return null;
+
+        await userRepository.update(id, dataUser);
+        return await userRepository.findOne({ where: { id } });
+    } catch (error) {
+        console.error("Error al actualizar el usuario:", error);
+    }
+}
+
+export async function deleteUserService(id) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+        const userFound = await userRepository.findOne({ where: { id } });
+
+        if (!userFound) return null;
+
+        const userDeleted = await userRepository.remove(userFound);
+        return userDeleted;
+    } catch (error) {
+        console.error("Error al eliminar el usuario:", error);
+    }
+}
