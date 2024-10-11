e90d2594bfdd2a48283e9a1f780c8551c578b696
BenjaminAGH <benjamin.guajardo2101@alumnos.ubiobio.cl>
2024-10-10 21:15:57 -0300

    Implementación Tarea

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -37,3 +37,63 @@ export async function getUserService(id) {
         console.error("Error al obtener el usuario:", error);
     }
 }
+
+export async function getUsersService() {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const users = await userRepository.find();
+
+        if (!users || users.length === 0) {
+            return null;
+        }
+
+        return users;
+    } catch (error) {
+        console.error("Error al obtener los usuarios:", error);
+    }
+}
+
+export async function updateUserService(id, dataUser) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const userFound = await userRepository.findOne({
+            where: { id }
+        });
+
+        if (!userFound) {
+            return null;
+        }
+
+        userFound.nombreCompleto = dataUser.nombreCompleto;
+        userFound.rut = dataUser.rut;
+        userFound.email = dataUser.email;
+
+        const userUpdated = await userRepository.save(userFound);
+
+        return userUpdated;
+    } catch (error) {
+        console.error("Error al actualizar el usuario:", error);
+    }
+}
+
+export async function deleteUserService(id) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const userFound = await userRepository.findOne({
+            where: { id }
+        });
+
+        if (!userFound) {
+            return null;
+        }
+
+        await userRepository.remove(userFound);
+
+        return true;
+    } catch (error) {
+        console.error("Error al eliminar el usuario:", error);
+    }
+}
